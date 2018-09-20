cb96388cde29f3254e2cb9f91da66d4a193225ec
rl1987 <rl1987@sdf.lonestar.org>
2018-09-20 16:44:48 +0300

    Solve inheritance exercise

---
+++ AdvancedToken.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.17;
+
+contract BasicToken {
+    uint256 public totalSupply;
+    function balanceOf(address who) constant returns (uint256);
+    function transfer(address to, uint256 value) returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+contract AdvancedToken is BasicToken {
+    
+}
